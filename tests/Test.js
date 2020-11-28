d2c3d2e08400eca858ed5967d52c41358a1e1d59
Tylor <Zapblumar@gmail.com>
2020-11-28 13:28:25 -0700

    starting the tests

---
+++ tests/Test.js
@@ -0,0 +1,10 @@
+const jest = require("jest");
+
+test('creates an employment test', () => {
+
+
+    expect(Employee).toBe('');
+    expect(Engineer).toEqual('');
+    expect(Intern).toEqual('');
+    expect(Manager).toEqual('');
+});
